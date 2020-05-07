97563002991b28098109ab7ae27b000b31e25aa4
charlesc18 <charlesc18@vt.edu>
2020-05-07 17:42:28 -0400

    more setup directories

--- activre/src/index.js
+++ activre/src/index.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import ReactDOM, { render } from 'react-dom';
-import Home from './Home';
+import Main from './pages/main/Main';
 
 class App extends React.Component {
     render() {
         return(
             <div>
-                <Home />
+                <Main />
             </div>
         );
     }c
---
+++ activre/src/pages/main/Main.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './Main.css';
+
+class Main extends React.Component{
+    render(){
+        return(
+            <div>
+                <section id="header"></section>
+            </div>          
+        );
+    }
+};
+
+export default Main;
