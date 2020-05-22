7d04c70711cc3f319f4a9919c772cc6f19be72a8
charlesc18 <charlesc18@vt.edu>
2020-05-22 02:29:05 -0400

    finished landing page front-end

--- client/src/components/modal/modal.js
+++ client/src/components/modal/modal.js
@@ -5,12 +5,12 @@ import "./modal.css";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faGoogle } from "@fortawesome/free-brands-svg-icons";
 
-const modalStyles = {
+const loginStyles = {
   modal: {
     background: "#dbedf3",
     margin: "auto auto",
     width: "350px",
-    height: "375px",
+    height: "385px",
     "max-width": "400px",
     "max-height": "450px",
     border: "3px solid #404b69",
@@ -25,30 +25,91 @@ const modalStyles = {
   },
 };
 
-export default function ModalPopup({ open, setOpen }) {
-  return (
-    <div>
-      <Modal open={open} onClose={() => setOpen(false)} styles={modalStyles}>
-        <div className="container">
-          <div className="title">
-            <h2>Log in</h2>
+const signupStyles = {
+  modal: {
+    background: "#dbedf3",
+    margin: "auto auto",
+    width: "350px",
+    height: "475px",
+    "max-width": "400px",
+    "max-height": "600px",
+    border: "3px solid #404b69",
+    "border-radius": "10px",
+  },
+  closeButton: {
+    outline: "none",
+    border: "2px solid black",
+    padding: "2px 2px",
+    "box-shadow": "0px 1px black",
+    "border-radius": "100px",
+  },
+};
+
+export default function Popup({ open, setOpen, sign, setSign }) {
+  if (open && !sign) {
+    return (
+      <div>
+        <Modal
+          open={open}
+          onClose={() => (setOpen(false), setSign(false))}
+          styles={loginStyles}
+        >
+          <div className="container">
+            <div className="title">
+              <h2>Log in</h2>
+            </div>
+            <div className="google">
+              <FontAwesomeIcon icon={faGoogle} className="icon" />
+              <button>Login with Google</button>
+            </div>
+            <div className="inputs">
+              <p>Email:</p>
+              <input className="text-input" placeholder="Enter Email..." type="text" />
+              <p>Password:</p>
+              <input className="text-input" placeholder="Enter Password..." type="password" />
+              <a href="/">Forgot Password</a>
+            </div>
+            <div className="confirm">
+              <button className="modalButton">Confirm</button>
+            </div>
           </div>
-          <div className="google">
-            <FontAwesomeIcon icon={faGoogle} className="icon" />
-            <button>Login with Google</button>
+        </Modal>
+      </div>
+    );
+  } else if (open && sign) {
+    return (
+      <div>
+        <Modal
+          open={open}
+          onClose={() => (setOpen(false), setSign(false))}
+          styles={signupStyles}
+        >
+          <div className="container">
+            <div className="title">
+              <h2>Sign Up</h2>
+            </div>
+            <div className="google">
+              <FontAwesomeIcon icon={faGoogle} className="icon" />
+              <button>Sign up with Google</button>
+            </div>
+            <div className="inputs">
+              <p>Username:</p>
+              <input className="text-input" placeholder="Enter Username..." type="text" />
+              <p>Email:</p>
+              <input className="text-input" placeholder="Enter Email..." type="text" />
+              <p>Password:</p>
+              <input className="text-input" placeholder="Enter Password..." type="password" />
+              <p>Confirm Password:</p>
+              <input className="text-input" placeholder="Confirm Password..." type="password" />
+            </div>
+            <div className="confirm">
+              <button className="modalButton">Confirm</button>
+            </div>
           </div>
-          <div className="inputs">
-            <p>Email:</p>
-            <input placeholder="Enter Email..." type="text" />
-            <p>Password:</p>
-            <input placeholder="Enter Password..." type="password" />
-            <a href="/">Forgot Password</a>
-          </div>
-          <div className="confirm">
-            <button className="modalButton">Confirm</button>
-          </div>
-        </div>
-      </Modal>
-    </div>
-  );
+        </Modal>
+      </div>
+    );
+  } else {
+    return <div></div>;
+  }
 }
--- client/src/pages/landing/screen.js
+++ client/src/pages/landing/screen.js
@@ -1,33 +1,45 @@
 import React, { useState } from "react";
 import "./startup.css";
-import ModalPopup from "../../components/modal/modal";
+import Popup from "../../components/modal/modal";
 
 export default function Landing() {
   const [open, setOpen] = useState(false);
+  const [sign, setSign] = useState(false);
   return (
     <div className="startup-container">
       <div className="startup-header">
         <p>activRE</p>
       </div>
-
+      <div className="modal-container">
+        <Popup setOpen={setOpen} open={open} setSign={setSign} sign={sign} />
+      </div>
       <div className="startup-body">
         <div className="description">
           <p>
-            Active Recall Studying Technique will help you remember discrete
-            facts and information in no time. Try it out now! Lorem ipsum dolor
-            sit amet, consectetur adipiscing elit, sed do eiusmod tempor
-            incididunt ut labore et dolore magna aliqua. Ut enim ad minim
-            veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex
-            ea commodo consequat.
+            <b>Most Effective Studying Technique!</b>
+            <br />
+            • Study shows that the active recall studying technique will help
+            you remember discrete facts and information with ease. <br />
+            • This website is dedicated to giving everyone an easy way to
+            practice this technique. Try it out now! <br /> • Creating modules,
+            Study with Spaced Repetition, Success in your field!
           </p>
         </div>
         <div className="login-button">
-          <button onClick={() => setOpen(true)} className="button">
-            Login
+          <button
+            onClick={() => (setOpen(true), setSign(false))}
+            className="button"
+          >
+            Log in
           </button>
         </div>
         <div className="signup-button">
-          <button className="button">Sign Up</button>
+          <button
+            onClick={() => (setOpen(true), setSign(true))}
+            className="button"
+          >
+            Sign Up
+          </button>
         </div>
       </div>
 
@@ -35,10 +47,6 @@ export default function Landing() {
         <p>
           <b>© 2020 activRE, All Rights Reserved</b>
         </p>
-      </div>
-
-      <div className="modal-container">
-        <ModalPopup setOpen={setOpen} open={open} />
       </div>
     </div>
   );
