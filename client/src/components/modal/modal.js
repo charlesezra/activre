8de475691c3218eaebb978b208a860de02b488f3
charlesc18 <charlesc18@vt.edu>
2020-05-21 20:47:40 -0400

    finished login frontend component

--- client/src/components/modal/modal.js
+++ client/src/components/modal/modal.js
@@ -1,14 +1,27 @@
 import React from "react";
 import { Modal } from "react-responsive-modal";
 import "react-responsive-modal/styles.css";
+import "./modal.css";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faGoogle } from "@fortawesome/free-brands-svg-icons";
 
 const modalStyles = {
   modal: {
-    background: "lightblue",
+    background: "#dbedf3",
     margin: "auto auto",
     width: "350px",
+    height: "375px",
     "max-width": "400px",
-    border: "2px solid white",
+    "max-height": "450px",
+    border: "3px solid #404b69",
+    "border-radius": "10px",
+  },
+  closeButton: {
+    outline: "none",
+    border: "2px solid black",
+    padding: "2px 2px",
+    "box-shadow": "0px 1px black",
+    "border-radius": "100px",
   },
 };
 
@@ -16,15 +29,25 @@ export default function ModalPopup({ open, setOpen }) {
   return (
     <div>
       <Modal open={open} onClose={() => setOpen(false)} styles={modalStyles}>
-        <h2 className="title">Login</h2>
-        <div className="Inputs">
-          <p>Email:</p>
-          <input placeholder="Enter Email..."></input>
-          <p>Password:</p>
-          <input placeholder="Enter Password..."></input>
-          <button>Confirm</button>
+        <div className="container">
+          <div className="title">
+            <h2>Log in</h2>
+          </div>
+          <div className="google">
+            <FontAwesomeIcon icon={faGoogle} className="icon" />
+            <button>Login with Google</button>
+          </div>
+          <div className="inputs">
+            <p>Email:</p>
+            <input placeholder="Enter Email..." type="text" />
+            <p>Password:</p>
+            <input placeholder="Enter Password..." type="password" />
+            <a href="/">Forgot Password</a>
+          </div>
+          <div className="confirm">
+            <button className="modalButton">Confirm</button>
+          </div>
         </div>
-        <button>Login with Google</button>
       </Modal>
     </div>
   );
