e1043bcbdb831a99c98532461104fd585e64ea80
charlesc18 <charlesc18@vt.edu>
2020-08-22 16:20:13 -0400

    Updated README.md

---
+++ README.md
@@ -0,0 +1,27 @@
+## activRE
+
+The purpose of this application is to be able to use the active recall studying technique through a web app. I noticed that people use this technique with Google Sheets all manually, and I wanted to make a dedicated web app that will make it easier for the user to create these components.
+
+Also, this is a working project and it is not done yet. I will continue to work on this throughout the fall semester and I am projecting that I will have a working prototype towards the end. I hope it will impact students' studying when it is released.
+
+I am continuously learning the technologies being used through online documentation and tutorials.
+
+Learning more about:
+* JavaScript
+* Web Development - Full Stack
+* Containerization
+* Server Architecture
+
+---
+
+## Technologies
+
+* ReactJS - FrontEnd
+* Node.js w/ Express - BackEnd
+* MongoDB - Database
+
+---
+
+## Screenshots
+
+### To Be Continued ...
--- client/src/components/form/form.js
+++ client/src/components/form/form.js
@@ -4,7 +4,26 @@ import "./form.css";
 export default function Form() {
     return(
         <div className="form-container">
-            <p>Hello World</p>
+            <form>
+                <div className="title-input-container">
+                    <label>Title</label>
+                </div>
+                <div className="topic-input-container">
+                    <label>Topic Name</label>
+                </div>
+                <div className="question-input-container">
+                    <label>Question</label>
+                </div>
+                <div className="answer-input-container">
+                    <label>Answer</label>
+                </div>
+                <div className="color-input-container">
+                    <label>Level of Memorization</label>
+                </div>
+                <div className="scope-input-container">
+                    <label>Private/Public</label>
+                </div>
+            </form>
         </div>
     );
 }
